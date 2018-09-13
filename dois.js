97f78443365ffea81de730cacfed9d0079279282
Bob <negenentwintig@hotmail.com>
2018-09-13 22:42:04 +0200

    additional data

--- dois.js
+++ dois.js
@@ -3,14 +3,11 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 
 let data = JSON.parse(fs.readFileSync('titles-labels.json'))
-let flat = [].concat.apply([], data.map(d => d.rows.map(r=>({authors: r[0].trim(), title: r[1].trim(), result: r[9].trim()}))))
-    .filter(r => /\d/.test(r.result))
+let flat = [].concat.apply([], data.map(d => d.rows.map(r=>({authors: r[0].trim(), title: r[1].trim(), result: r[9].trim(), questioned: r[11].trim()}))))
+    .filter(r => !/\d/.test(r.result) && /\d/.test(r.questioned))
 console.log(flat)
 
 ;(async _ => {
-    const browser = await p.launch({})
-    const page = await browser.newPage()
-    page.on('console', console.log)
     process.on("unhandledRejection", e => {
         // console.log(await page.$eval('html', x => x ? x.innerText : ''))
         throw e
@@ -31,19 +28,4 @@ console.log(flat)
 
     fs.writeFileSync('flat.json', JSON.stringify({data: flat, dois}))
     console.log(dois)
-    return
-    for (let t of flat) {
-        await page.goto('https://sci-hub.tw')
-        let input = await page.$('input[name=request]')
-        let search = t.authors + ' ' + t.title
-
-        console.log(search)
-        await input.type(search)
-        await page.waitFor(2000)
-        await input.press('Enter')
-        await page.waitForNavigation()
-        await page.waitFor(2000)
-        let pdf = await page.$eval('#pdf', pdf => pdf.src)
-        console.log(pdf)
-    }
 })()
